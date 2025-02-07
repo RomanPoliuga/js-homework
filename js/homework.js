0fd5f7c308518eccec79313fee00a8efd8424298
RomanPoliuga <161931643+RomanPoliuga@users.noreply.github.com>
2025-02-07 19:14:33 +0200

    дз

---
+++ js/homework.js
@@ -0,0 +1,30 @@
+const age = '15';
+console.log(age)
+
+const Name = 'Roman'; 
+console.log(Name)
+
+const isStudent = false;
+console.log(isStudent)
+
+const myString = 'Велика сила несе за собою велику відповідальність.';
+console.log(myString)
+
+let myNumber = 45;
+myNumber = +10;
+console.log(myNumber)
+
+const myNull = null;
+console.log(myNull)
+
+const myName = "Введіть своє ім’я";
+const userName = prompt(myName)
+alert(userName)
+console.log(typeof userName)
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?")
+console.log(userAnswer)
+
+alert("Увага! Дія може бути небезпечною.")
+const shouldrennu = confirm("“Ви впевнені, що хочете продовжити?")
+console.log(shouldrennu)
